25f74a058e4fbdfa8ba323dc7227e88dac2d3994
yarogniew <1@yarogniew.net>
2018-03-10 00:06:18 +0100

    wyczyszczone z muzyką

--- pointillism.js
+++ pointillism.js
@@ -1,14 +1,18 @@
 var img;
+var song;
 var smallPoint, largePoint;
 
 function preload() {
   img = loadImage("Jaga_tan3.png");
+  song = loadSound('Potrzeby.mp3');
 }
 
 function setup() {
+
+  song.play();
   createCanvas(420, 578);
   smallPoint = 4;
-  largePoint = 40;
+  largePoint = 20;
   imageMode(CENTER);
   noStroke();
   background(255);
@@ -16,6 +20,7 @@ function setup() {
 }
 
 function draw() {
+
   var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
   var x = floor(random(img.width));
   var y = floor(random(img.height));
